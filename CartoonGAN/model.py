60d67337d772b66576f3df942e0eecce3e3dddba
Crie <crosssceneofwindff@gmail.com>
2018-09-21 22:19:40 +0900

    update

--- CartoonGAN/model.py
+++ CartoonGAN/model.py
@@ -2,16 +2,16 @@ import chainer
 import chainer.links as L
 import chainer.functions as F
 from chainer import cuda, Chain, initializers
+from instance_normalization import InstanceNormalization
 
 class VGG(Chain):
-    def __init__(self, in_ch, out_ch):
+    def __init__(self):
         super(VGG,self).__init__()
         with self.init_scope():
-            self.base = L.VGG19Layers()
+            self.base = L.VGG16Layers()
 
     def __call__(self, x):
-        h = self.base(x, layers=["conv4_4"])["conv4_4"]
-        h.unchain_backward()
+        h = self.base(x, layers=["conv4_3"])["conv4_3"]
 
         return h
 
@@ -20,14 +20,14 @@ class CCBR_down(Chain):
         super(CCBR_down, self).__init__()
         w = initializers.Normal(0.02)
         with self.init_scope():
-            self.c0 = L.Convotluion2D(in_ch. out_ch,3,2,1,initialW = w)
+            self.c0 = L.Convolution2D(in_ch, out_ch,4,2,1,initialW = w)
             self.c1 = L.Convolution2D(out_ch, out_ch,3,1,1,initialW = w)
 
-            self.bn0 = L.BatchNormalization(out_ch)
+            self.bn0 = InstanceNormalization(out_ch)
 
     def __call__(self,x):
         h =self.c0(x)
-        h =F.relu(self.bn(self.c0(h)))
+        h =F.relu(self.bn0(self.c1(h)))
 
         return h
 
@@ -36,27 +36,28 @@ class CCBR_up(Chain):
         super(CCBR_up, self).__init__()
         w = initializers.Normal(0.02)
         with self.init_scope():
-            self.dc0 = L.Deconvolution2D(in_ch, out_ch, 3,2,1,initialW = w)
-            self.dc1 = L.Deconvolution2D(out_ch, out_ch, 3,1,1,initialW = w)
+            self.dc0 = L.Convolution2D(in_ch, out_ch, 3,1,1,initialW = w)
+            self.dc1 = L.Convolution2D(out_ch, out_ch, 3,1,1,initialW = w)
 
-            self.bn0 = L.BatchNormalization(out_ch)
+            self.bn1 = InstanceNormalization(out_ch)
 
     def __call__(self,x):
-        h = self.dc0(x)
-        h = F.relu(self.bn0(self.dc1(x)))
+        h = x
+        h = self.dc0(F.unpooling_2d(h, 2,2,0,cover_all=False))
+        h = F.relu(self.bn1(self.dc1(h)))
 
         return h
 
 class Resblock(Chain):
     def __init__(self, in_ch, out_ch):
-        super(ResBlock, self).__init__()
+        super(Resblock, self).__init__()
         w = initializers.Normal(0.02)
         with self.init_scope():
             self.c0 = L.Convolution2D(in_ch, out_ch, 3,1,1,initialW = w)
             self.c1 = L.Convolution2D(out_ch, out_ch, 3,1,1,initialW = w)
 
-            self.bn0 = L.BatchNormalization(out_ch)
-            self.bn1 = L.BatchNormalization(out_ch)
+            self.bn0 = InstanceNormalization(out_ch)
+            self.bn1 = InstanceNormalization(out_ch)
 
     def __call__(self,x):
         h = F.relu(self.bn0(self.c0(x)))
@@ -76,23 +77,33 @@ class Generator(Chain):
             self.res1 = Resblock(base*4, base*4)
             self.res2 = Resblock(base*4, base*4)
             self.res3 = Resblock(base*4, base*4)
+            self.res4 = Resblock(base*4, base*4)
+            self.res5 = Resblock(base*4, base*4)
+            self.res6 = Resblock(base*4, base*4)
+            self.res7 = Resblock(base*4, base*4)
             self.up0 = CCBR_up(base*4, base*2)
             self.up1 = CCBR_up(base*2, base)
             self.c1 = L.Convolution2D(base, 3,7,1,3,initialW = w)
 
-            self.bn0 = L.BatchNormalization(base)
+            self.bn0 = InstanceNormalization(base)
 
     def __call__(self,x):
-        h = F.relu(self.bn0(self.co(x)))
+        h = F.relu(self.bn0(self.c0(x)))
         h = self.down0(h)
         h = self.down1(h)
         h = self.res0(h)
         h = self.res1(h)
         h = self.res2(h)
         h = self.res3(h)
+        h = self.res4(h)
+        h = self.res5(h)
+        h = self.res6(h)
+        h = self.res7(h)
         h = self.up0(h)
         h = self.up1(h)
         h = self.c1(h)
+
+        return F.tanh(h)
 
 class Discriminator(Chain):
     def __init__(self, base=32):
@@ -100,16 +111,16 @@ class Discriminator(Chain):
         w = initializers.Normal(0.02)
         with self.init_scope():
             self.c0 = L.Convolution2D(3, base, 3,1,1,initialW = w)
-            self.c1 = L.Convolution2D(base, base*2, 3,2,1,initialW = w)
+            self.c1 = L.Convolution2D(base, base*2, 4,2,1,initialW = w)
             self.c2 = L.Convolution2D(base*2, base*4,3,1,1,initialW = w)
-            self.c3 = L.Convolution2D(base*4, base*4,3,2,1,initialW = w)
+            self.c3 = L.Convolution2D(base*4, base*4,4,2,1,initialW = w)
             self.c4 = L.Convolution2D(base*4, base*8,3,1,1,initialW = w)
             self.c5 = L.Convolution2D(base*8, base*8,3,1,1,initialW = w)
             self.c6 = L.Convolution2D(base*8, 1,3,1,1,initialW = w)
 
-            self.bn0 = L.BatchNormalization(base*4)
-            self.bn1 = L.BatchNormalization(base*8)
-            self.bn2 = L.BatchNormalization(base*8)
+            self.bn0 = InstanceNormalization(base*4)
+            self.bn1 = InstanceNormalization(base*8)
+            self.bn2 = InstanceNormalization(base*8)
 
     def __call__(self, x):
         h = F.leaky_relu(self.c0(x))
--- CartoonGAN/train.py
+++ CartoonGAN/train.py
@@ -5,13 +5,15 @@ from chainer import cuda, Chain, optimizers, initializers, serializers
 import os
 import numpy as np
 import argparse
+import pylab
 from model import Discriminator, Generator, VGG
+from prepare import prepare_trim_content, prepare_trim_style
 
 xp = cuda.cupy
 cuda.get_device(0).use()
 
-def set_optimzier(mode, alpha=0.0002, beta=0.5):
-    optimizer = optimizers.Adam(alpha=alpha, beta1 = beta)
+def set_optimizer(model, alpha=0.0002, beta=0.5, beta2 = 0.999):
+    optimizer = optimizers.Adam(alpha=alpha, beta1 = beta, beta2 = 0.999)
     optimizer.setup(model)
 
     return optimizer
@@ -22,17 +24,39 @@ if not os.path.exists(outdir):
 
 parser = argparse.ArgumentParser(description="cartoongan")
 parser.add_argument("--epochs",default=1000,type=int,help="the number of epochs")
-parser.add_argument("--batchsize",default=16,type=int,help="batchsize")
-parser.add_argument("--interval",default=5,type=int,help="the interval of snapshot")
+parser.add_argument("--batchsize",default=3,type=int,help="batchsize")
+parser.add_argument("--interval",default=1,type=int,help="the interval of snapshot")
 parser.add_argument("--cw",default=10.0,type=float,help="the weights of content loss")
+parser.add_argument("--Ntrain", default=20000, type = int, help = "Ntrain")
+parser.add_argument("--Nstyle",default = 4500, type = int, help = "Nstyle")
+parser.add_argument("--testsize", default = 2, type = int, help = "testsize")
 
 args=parser.parse_args()
 epochs = args.epochs
 batchsize = args.batchsize
 interval = args.interval
 content_weight = args.cw
+Ntrain = args.Ntrain
+Nstyle = args.Nstyle
+testsize = args.testsize
+
+image_path = "/usr/MachineLearning/Dataset/trim/coco_trim/"
+image_list = os.listdir(image_path)
+style_path = "/usr/MachineLearning/Dataset/macross/"
+smooth_path = "/usr/MachineLearning/Dataset/edge_smooth/"
+
+test_box = []
+for _ in range(testsize):
+    rnd = np.random.randint(Ntrain, Ntrain + 1000)
+    test_name = image_path + image_list[rnd]
+    test,_,_ = prepare_trim_content(test_name)
+    test_box.append(test)
+
+x_test = xp.array(test_box).astype(xp.float32)
+x_test = chainer.as_variable(x_test)
 
 generator = Generator()
+serializers.load_npz("./model/generator_pretrain_2.model", generator)
 generator.to_gpu()
 gen_opt = set_optimizer(generator)
 
@@ -42,17 +66,30 @@ dis_opt = set_optimizer(discriminator)
 
 vgg = VGG()
 vgg.to_gpu()
-vgg_opt.update()
+vgg_opt = set_optimizer(vgg)
 vgg.base.disable_update()
 
 for epoch in range(epochs):
     sum_gen_loss = 0
     sum_dis_loss = 0
-    for batch in range(0,Ntrain,batchsize):
+    for batch in range(0, 2000, batchsize):
         image_box = []
         style_box = []
         smooth_box = []
         for i in range(batchsize):
+            rnd1 = np.random.randint(Ntrain)
+            image_name = image_path + image_list[rnd1]
+            image,_,_ = prepare_trim_content(image_name)
+            image_box.append(image)
+
+            rnd2 = np.random.randint(Nstyle)
+            style_name = style_path + "macross_" + str(rnd2) + ".png"
+            style,width,height = prepare_trim_content(style_name)
+            style_box.append(style)
+
+            smooth_name = smooth_path + "edge_smooth" + str(rnd2) + ".png"
+            smooth_edge = prepare_trim_style(smooth_name,width,height)
+            smooth_box.append(smooth_edge)
         
         x = xp.array(image_box).astype(xp.float32)
         t = xp.array(style_box).astype(xp.float32)
@@ -79,9 +116,8 @@ for epoch in range(epochs):
 
         y = generator(x)
         y_dis = discriminator(y)
-        s_dis = discriminator(s)
-        gen_loss = F.mean(F.softplus(-y_dis)) + F.mean(F.softplus(-s_dis))
-        content_loss = F.mean_absolute_error(vgg(t) - vgg(y))
+        gen_loss = F.mean(F.softplus(-y_dis))
+        content_loss = F.mean_absolute_error(vgg(x) , vgg(y))
         gen_loss += content_weight * content_loss
 
         generator.cleargrads()
@@ -89,8 +125,26 @@ for epoch in range(epochs):
         gen_opt.update()
         gen_loss.unchain_backward()
 
+        sum_dis_loss += dis_loss.data.get()
+        sum_gen_loss += gen_loss.data.get()
+
         if epoch % interval == 0 and batch == 0:
             serializers.save_npz("generator.model", generator)
             with chainer.using_config("train", False):
                 y_test = generator(x_test)
             y_test = y_test.data.get()
+            x_t = x_test.data.get()
+            for i in range(testsize):
+                tmp = (np.clip(x_t[i]*127.5 + 127.5,0,255)).transpose(1,2,0).astype(np.uint8)
+                pylab.subplot(testsize, 2, 2*i+1)
+                pylab.imshow(tmp)
+                pylab.axis("off")
+                pylab.savefig('%s/visualize_%d.png'%(outdir, epoch))
+                tmp = (np.clip(y_test[i]*127.5 + 127.5,0,255)).transpose(1,2,0).astype(np.uint8)
+                pylab.subplot(testsize, 2, 2*i+2)
+                pylab.imshow(tmp)
+                pylab.axis("off")
+                pylab.savefig('%s/visualize_%d.png'%(outdir, epoch))
+
+    print("epoch : {}".format(epoch))
+    print("Generator : {} Discriminator : {}".format(sum_gen_loss / Ntrain, sum_dis_loss / Ntrain))
